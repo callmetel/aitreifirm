4c18e1c1ad69c43d6c913b6853c6a46ad9da057a
Tel <tel@jsdfyodds.com>
2021-09-01 00:25:47 -0400

    create global our process section & styles

--- wp-content/themes/divi-child-theme/page-templates/page-template-home.php
+++ wp-content/themes/divi-child-theme/page-templates/page-template-home.php
@@ -10,6 +10,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 ?>
 
 <?php
+	get_template_part( 'partials/global', 'process' );
 	get_template_part( 'partials/global', 'comparison_chart' );
 ?>
 
