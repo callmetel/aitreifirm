384b1cf983e4189947d282e78809a97e68b42984
Tel <tel@jsdfyodds.com>
2021-09-22 21:45:09 -0400

    update & style home page template

--- wp-content/themes/divi-child-theme/page-templates/page-template-home.php
+++ wp-content/themes/divi-child-theme/page-templates/page-template-home.php
@@ -9,13 +9,6 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 
 ?>
 
-<?php
-	get_template_part( 'partials/global', 'process' );
-	get_template_part( 'partials/global', 'comparison_chart' );
-?>
-
-<?php /* if ( ! $is_page_builder_used ) : ?>
-
 <div id="main-content">
 
 	<?php while ( have_posts() ) : the_post(); ?>
@@ -46,47 +39,66 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 
 				<!-- Subpage Banner -->
 				<?php 
-					// Get Featured Image 
+					// Banner Image 
+					$imageUrl = site_url('/wp-content/uploads/home-bg.jpg');
+			  ?>
+				<style>
+					@media only screen and (min-width: 981px) {
+						.banner-home-img {
+							background-image: url('<?php echo $imageUrl; ?>');
+						}
+					}
+				</style>
 
-				    if ( has_post_thumbnail( $post->ID ) ) :
-				        $imageInfo = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-				        $imageUrl = $imageInfo[0];
-				    endif;
-				  ?>
-
-				<div class="et_pb_section sbpg-bnnr-sctn et_pb_with_background et_section_regular" style="background-image: url('<?php echo $imageUrl; ?>'); background-size: cover;">
-					<div class="sbpg-bnnr-rw stndrd-rw et_pb_row">
-						<div class="sbpg-bnnr-col et_pb_column et_pb_column_4_4">
-							<div class="sbpg-bnnr et_pb_text et_pb_module et_pb_bg_layout_light">
+				<div id="banner" class="banner banner-home banner-home-img et_pb_section hmpg-bnnr-sctn et_pb_with_background et_section_regular">
+					<div class="hmpg-bnnr-rw stndrd-rw et_pb_row">
+						<div class="hmpg-bnnr-col et_pb_column et_pb_column_1_2">
+							<div class="hmpg-bnnr et_pb_text et_pb_module et_pb_bg_layout_light">
 								<div class="et_pb_text_inner">
-									<h1><?php the_title(); ?></h1>	
+									<h1 class="page-title">
+										<span class="main-title">It’s time <br>to invest.</span>
+									</h1>	
+									<p class="sub-title">The primary goal at AIT is to aquire and rehab properties that replenish the neighborhoods we are apart of.</p>
+									<a href="<?php echo site_url('/options/'); ?>" class="btn">Learn More</a>
 								</div>
 							</div> <!-- .et_pb_text -->
 						</div> <!-- .et_pb_column -->
+						<div class="sbpg-bnnr-col et_pb_column et_pb_column_1_2 et_pb_column_empty"></div>
 					</div>
 				</div> 
 				<!-- End Subpage Banner -->
+				<!-- Partners Logos Section -->
+				<?php get_template_part( 'partials/global', 'partner_logos' ); ?>
 
 				<!-- Subpage Content -->
-				<div class="sbpg-cntnt-sctn et_pb_section et_section_regular">
-					<div class="sbpg-cntnt-rw stndrd-rw et_pb_row et_pb_equal_columns">
-
-						<!-- Subpage Left Content -->
-						<div class="sbpg-cntnt-col sbpg-cntnt-col-1 et_pb_column et_pb_column_2_3">
-							<div class="et_pb_text et_pb_module et_pb_bg_layout_light et_pb_text_align_left">
-								<div class="et_pb_text_inner">
-									<?php the_content(); ?>
-								</div>
-							</div>
-						</div> <!-- .et_pb_column -->
-
-						<!-- Subpage Right Sidebar -->
-						<div class="sbpg-cntnt-col sbpg-cntnt-col-1 et_pb_column et_pb_column_1_3">
-							<!-- Sidebar Divi Library Items -->
-							<?php echo do_shortcode('[et_pb_section global_module="25232"][/et_pb_section]');?>
-						</div> <!-- .et_pb_column -->
-					</div> <!-- .et_pb_row -->
+				<div id="about-us" class="sbpg-cntnt-sctn et_pb_section et_section_regular">
+				  <div class="sm-rw et_pb_row et_pb_equal_columns et_pb_gutters4">
+				    <div class="image-col et_pb_column et_pb_column_1_2 et_pb_css_mix_blend_mode_passthrough">
+				    	<style type="text/css">
+				    		@media only screen and (min-width: 981px) {
+					    		#about-us .image-col .image {
+					    			background-image: url("<?php echo site_url('/wp-content/uploads/pexels-vlada-karpovich.jpg'); ?>");
+					    		}
+					    	}
+				    	</style>
+				      <div class="image"></div>
+				    </div>
+				    <div class="text-col et_pb_column et_pb_column_1_2 et_pb_css_mix_blend_mode_passthrough et-last-child">
+				      <div class="text et_pb_text et_pb_module">
+				        <div class="et_pb_text_inner">
+				          <h3>About Us</h3>
+				          <p>AIT is a real estate investment firm ran and operated by a lean team with local personnel, partners, and associates that acquires and rehab real estate off market. Heavily invested in communiy, our goal  is to conduct an efficient and ethical business that replenishes the neighborhoods in which we’re apart of. Founded on principles to combat homelessness, poverty, climate change, and inequities. As we grow, the standard is to enhance and propel the quality of life for American neighborhoods throughout the nation.</p>
+				        </div>
+				      </div>
+				    </div>
+				  </div>
 				</div>
+
+				<!-- Our Process Section -->
+				<?php get_template_part( 'partials/global', 'process' ); ?>
+
+				<!-- Comparison Section -->
+				<?php get_template_part( 'partials/global', 'comparison_chart' ); ?>
 			</div> <!-- .entry-content -->
 
 		<?php
@@ -98,52 +110,5 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 	<?php endwhile; ?>
 
 </div> <!-- #main-content -->
-
-<?php endif; ?>
-
-<?php if ( $is_page_builder_used ) : ?>
-
-	<?php while ( have_posts() ) : the_post(); ?>
-
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-		<?php if ( ! $is_page_builder_used ) : ?>
-
-			<h1 class="entry-title main_title"><?php the_title(); ?></h1>
-		<?php
-			$thumb = '';
-
-			$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
-
-			$height = (int) apply_filters( 'et_pb_index_blog_image_height', 675 );
-			$classtext = 'et_featured_image';
-			$titletext = get_the_title();
-			$thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Blogimage' );
-			$thumb = $thumbnail["thumb"];
-
-			if ( 'on' === et_get_option( 'divi_page_thumbnails', 'false' ) && '' !== $thumb )
-				print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
-		?>
-
-		<?php endif; ?>
-
-			<div class="entry-content">
-			<?php
-				the_content();
-
-				if ( ! $is_page_builder_used )
-					wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
-			?>
-			</div> <!-- .entry-content -->
-
-		<?php
-			if ( ! $is_page_builder_used && comments_open() && 'on' === et_get_option( 'divi_show_pagescomments', 'false' ) ) comments_template( '', true );
-		?>
-
-		</article> <!-- .et_pb_post -->
-
-	<?php endwhile; ?>
-
-<?php endif; */?>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/divi-child-theme/partials/global-partner_logos.php
@@ -0,0 +1,21 @@
+<div id="partner-logos" class="prefooter-sctn et_pb_section et_section_regular">
+  <div class="stndrd-rw et_pb_row et_pb_gutters1">
+    <div class="et_pb_column et_pb_column_1_4 et_pb_css_mix_blend_mode_passthrough">
+      <div class="partner-logos-title-wrap et_pb_text et_pb_module et_pb_text_align_left">
+        <div class="et_pb_text_inner">
+        	<p class="partner-logos-title">Proud Partners of</p>
+        </div>
+      </div>
+    </div>
+    <div class="et_pb_column et_pb_column_3_4 et_pb_css_mix_blend_mode_passthrough">
+      <div class="partner-logos et_pb_code et_pb_module">
+        <div class="et_pb_code_inner">
+        	<div class="logo"><img src="<?php echo site_url('/wp-content/uploads/logo-1.png'); ?>" alt=""></div>
+        	<div class="logo"><img src="<?php echo site_url('/wp-content/uploads/logo-2.png'); ?>" alt=""></div>
+        	<div class="logo"><img src="<?php echo site_url('/wp-content/uploads/logo-3.png'); ?>" alt=""></div>
+        	<div class="logo"><img src="<?php echo site_url('/wp-content/uploads/logo-4.png'); ?>" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
