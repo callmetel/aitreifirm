5acdb06870f8263774fb3c2f84b120905841005c
Tel <tel@jsdfyodds.com>
2021-09-29 17:26:55 -0400

    add onload wrap to functions

--- login.js
+++ login.js
@@ -1,2 +1,4 @@
-document.getElementById("user_login").setAttribute("placeholder","Username/Email");
-document.getElementById("user_pass").setAttribute("placeholder","Password");
+window.onload = function() {
+	document.getElementById("user_login").setAttribute("placeholder","Username/Email");
+	document.getElementById("user_pass").setAttribute("placeholder","Password");
+}
