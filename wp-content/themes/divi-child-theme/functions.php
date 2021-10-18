3c2ce16c8c618539304d313c0f1de8d38429f912
Tel <tel@jsdfyodds.com>
2021-10-17 22:35:06 -0400

    show admin menu pages

--- wp-content/themes/divi-child-theme/functions.php
+++ wp-content/themes/divi-child-theme/functions.php
@@ -258,4 +258,20 @@ function admin_style() {
   wp_enqueue_style('admin-styles', get_stylesheet_directory_uri().'/admin.css');
 }
 add_action('admin_enqueue_scripts', 'admin_style');
+
+// Show Admin Menu Pages 
+if (!function_exists('debug_admin_menus')):
+function debug_admin_menus() {
+if ( !is_admin())
+        return;
+    global $submenu, $menu, $pagenow;
+    if ( current_user_can('manage_options') ) { // ONLY DO THIS FOR ADMIN
+        if( $pagenow == 'index.php' ) {  // PRINTS ON DASHBOARD
+            echo '<pre>'; print_r( $menu ); echo '</pre>'; // TOP LEVEL MENUS
+            echo '<pre>'; print_r( $submenu ); echo '</pre>'; // SUBMENUS
+        }
+    }
+}
+add_action( 'admin_notices', 'debug_admin_menus' );
+endif;
 ?>
