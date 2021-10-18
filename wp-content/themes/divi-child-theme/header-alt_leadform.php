ccce70f06d67fc50125c8f7e6ab6d4cac1ee7627
Tel <tel@jsdfyodds.com>
2021-10-18 00:50:26 -0400

    add favicon code to header files

--- wp-content/themes/divi-child-theme/header-alt_leadform.php
+++ wp-content/themes/divi-child-theme/header-alt_leadform.php
@@ -21,6 +21,14 @@
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
 	<?php $template_directory_uri = get_template_directory_uri(); ?>
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( $template_directory_uri . '/js/html5.js"' ); ?>" type="text/javascript"></script>
